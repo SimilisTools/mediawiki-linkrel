8f72408512620e2c75118100a7fd19facfceff0c
Toni Hermoso Pulido <toniher@cau.cat>
2014-12-01 01:47:18 +0100

    guide for rel

--- Linkrel_body.php
+++ Linkrel_body.php
@@ -11,7 +11,19 @@ class Linkrel {
 	 */
 	public static function process_linkrel( &$parser, $frame, $args ) {
 
-		return '';
+		// Get output from Parser
+		$out = $parser->getOutput();
+		
+		// TODO: Improvet this depending params
+		$attrs = array();
+		/** Params
+		  * rel = prev, next, canonical
+		  * href = URL
+		  * If not url, offset ( e. g. offset=page ) -> Process current url depending on the offset, check integer, and so on...
+		**/
+		
+		$out->addLink( $attrs );
+		return true;
 	}
 
 
