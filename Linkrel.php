83f4a18e0a12d13cbc094f7496e5c11c1a383e4a
Toni Hermoso Pulido <toniher@cau.cat>
2015-09-26 16:49:58 +0200

    link rel

--- Linkrel.php
+++ Linkrel.php
@@ -5,22 +5,26 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
 }
 
-$GLOBALS['wgExtensionCredits']['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'Linkrel',
-	'version' => '0.1',
-	'url' => 'https://github.com/SimilisTools/mediawiki-linkrel',
-	'author' => array( 'Toniher' ),
-	'descriptionmsg' => 'linkrel-desc',
-);
+//self executing anonymous function to prevent global scope assumptions
+call_user_func( function() {
 
-$GLOBALS['wgAutoloadClasses']['Linkrel'] = __DIR__.'/Linkrel_body.php';
-$GLOBALS['wgMessagesDirs']['Linkrel'] = __DIR__ . '/i18n';
-$GLOBALS['wgExtensionMessagesFiles']['Linkrel'] = __DIR__ . '/Linkrel.i18n.php';
-$GLOBALS['wgExtensionMessagesFiles']['LinkrelMagic'] = __DIR__ . '/Linkrel.i18n.magic.php';
+	$GLOBALS['wgExtensionCredits']['parserhook'][] = array(
+		'path' => __FILE__,
+		'name' => 'Linkrel',
+		'version' => '0.1',
+		'url' => 'https://github.com/SimilisTools/mediawiki-linkrel',
+		'author' => array( 'Toniher' ),
+		'descriptionmsg' => 'linkrel-desc',
+	);
+	
+	$GLOBALS['wgAutoloadClasses']['Linkrel'] = __DIR__.'/Linkrel_body.php';
+	$GLOBALS['wgMessagesDirs']['Linkrel'] = __DIR__ . '/i18n';
+	$GLOBALS['wgExtensionMessagesFiles']['Linkrel'] = __DIR__ . '/Linkrel.i18n.php';
+	$GLOBALS['wgExtensionMessagesFiles']['LinkrelMagic'] = __DIR__ . '/Linkrel.i18n.magic.php';
+	
+	$GLOBALS['wgHooks']['ParserFirstCallInit'][] = 'wfRegisterLinkrel';
 
-$GLOBALS['wgHooks']['ParserFirstCallInit'][] = 'wfRegisterLinkrel';
-
+});
 
 /**
  * @param $parser Parser
